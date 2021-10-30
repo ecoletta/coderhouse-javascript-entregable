360e161d18b33ec23b821f34d8a4dc51a59aaae0
COLETTA EZEQUIEL <ecoletta@santandertecnologia.com.ar>
2021-10-30 16:40:59 -0300

    Se agrega el entregable 1

---
+++ Clase01-E1/scriptPromedio.js
@@ -0,0 +1,17 @@
+// Le pido al usuario el valor 1
+let num1 = parseInt(prompt("Ingrese el valor 1"));
+// Le pido al usuario el valor 2
+let num2 = parseInt(prompt("Ingrese el valor 2"));
+// Le pido al usuario el valor 3
+let num3 = parseInt(prompt("Ingrese el valor 3"));
+// Le pido al usuario el valor 4
+let num4 = parseInt(prompt("Ingrese el valor 4"));
+// Sumo los 4 valores
+let resultado = num1 + num2 + num3 + num4;
+// Divido por 4 el resultado
+let promedio = resultado / 4;
+// Le envio al usuario el resultado
+alert(promedio);
+// Pongo el resultado en consola
+console.log(promedio);
+
