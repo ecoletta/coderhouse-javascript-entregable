ad4672fcd325d17a01cbe53dfe86707fc1d08a98
COLETTA EZEQUIEL <ecoletta@santandertecnologia.com.ar>
2021-10-30 16:34:25 -0300

    Primer commit

---
+++ Clase02-E2/script.js
@@ -0,0 +1,6 @@
+let numero = parseInt( prompt("Ingrese un numero, el rango aceptable es un numero menor a 1000"));
+if (numero <= 1000){
+    alert("El numero esta dentro del rango aceptado");
+}else{
+    alert("El numero esta fuera del rango aceptado");
+};
