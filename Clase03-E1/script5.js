8224c29829fd0e6060a4ee3d314c312b5998784f
COLETTA EZEQUIEL <ecoletta@santandertecnologia.com.ar>
2021-11-04 19:31:26 -0300

    Se modifica utilizando el do while como recomiendan

--- Clase03-E1/script5.js
+++ Clase03-E1/script5.js
@@ -1,8 +1,10 @@
-let palabraIngresada = prompt("Ingrese una palabra. Escriba ESC para salir");
-let palabrasConcatenadas ="";
-while(palabraIngresada != "ESC"){
-    palabrasConcatenadas = palabrasConcatenadas + "; " + palabraIngresada; 
+let palabraIngresada="";
+let palabrasConcatenadas="";
+do{
+    if(palabraIngresada === ""){
+    }else{
+        palabrasConcatenadas = palabrasConcatenadas + "; " + palabraIngresada; }
     palabraIngresada = prompt("Ingrese una palabra. Escriba ESC para salir");
-}
+}while(palabraIngresada != "ESC")
 console.log(palabrasConcatenadas);
 alert("El programa ha finalizado");
